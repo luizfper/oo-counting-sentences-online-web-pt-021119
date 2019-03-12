3aaec6dd9d1b7da589c87f8d2e39ce96f200b388
luizfper <luizfper@gmail.com>
2019-03-11 20:25:38 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    !!self.match(/[.?!]/)
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.scan(/\b[!.?]/).size
   end
 end
